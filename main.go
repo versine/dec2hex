84fc9f1ae34f1e1c024facee516eb5ebb4546fcf
David Baerg <versine@protonmail.com>
2017-03-23 14:48:37 -0400

    Add flag to insert spaces between output numbers

--- main.go
+++ main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"strconv"
 )
 
 func main() {
-	if len(os.Args) < 2 {
+	s := flag.Bool("s", false, "Whether to insert spaces between individual numbers")
+	flag.Parse()
+
+	if len(flag.Args()) < 2 {
 		fmt.Printf("Usage %s n1 n2 n3 ...", os.Args[0])
 	}
-	nums := os.Args[1:]
+
+	nums := flag.Args()
 	for _, num := range nums {
 		n, _ := strconv.Atoi(num)
-		fmt.Printf("%x", n)
+		fmt.Printf("%02x", n)
+		if *s {
+			fmt.Printf(" ")
+		}
 	}
 	fmt.Printf("\n")
 }
